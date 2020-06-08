fab81e03e83e46fa336ae4bd7dd45141da756e95
gbubemismith <gsmithwalter@gmail.com>
2020-06-08 23:37:10 +0100

    Added page filters to SPA

--- src/app/members/member-list/member-list.component.ts
+++ src/app/members/member-list/member-list.component.ts
@@ -14,6 +14,9 @@ import { Pagination } from 'src/app/models/pagination';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  user: User =  JSON.parse(localStorage.getItem('user'));
+  genderList =  [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {};
   pagination: Pagination;
 
 
@@ -25,16 +28,32 @@ export class MemberListComponent implements OnInit {
       this.users = data['users'].result;
       this.pagination = data['users'].pagination;
     });
-    
+
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+
   }
 
   pageChanged(event: any) {
     this.pagination.currentPage = event;
     this.loadUsers();
   }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+
+    this.loadUsers();
+  }
  
   loadUsers() {
-    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+    console.log(this.userParams.orderBy);
+
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
     .subscribe((res: PaginatedResult<User[]>) => {
       this.users = res.result;
       this.pagination = res.pagination;
